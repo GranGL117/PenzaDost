0b1ae3163b6b77858ebde02073ad8ead30bdf5e3
GranGL117 <grangl117@gmail.com>
2022-01-31 17:41:34 +0300

    Удалил about.php. Сделал адаптив. 31.01.22

--- Dost/content.php
+++ Dost/content.php
@@ -10,6 +10,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://fonts.cdnfonts.com/css/gotham" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style.css?=version5">
@@ -28,7 +29,6 @@
             <nav class="nav_">
                 <a class="nav_link" href="../index.php">Главная</a>
                 <a class="nav_link" href="../map.php">Карта</a>
-                <a class="nav_link" href="../about.php">О сайте</a>
             </nav>
         </div>
     </div>
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://fonts.cdnfonts.com/css/gotham" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css?=version5">
@@ -15,12 +16,11 @@
         <div class="header_inner">
             <div class="header_logo">
                 <img id="logo" src="img/logo.png">
-
             </div>
+            <h1 class="header_title">Достопримечательности Пензы</h1>
             <nav class="nav_">
                 <a class="nav_link" href="index.php">Главная</a>
                 <a class="nav_link" href="map.php">Карта</a>
-                <a class="nav_link" href="about.php">О сайте</a>
             </nav>
         </div>
     </div>
